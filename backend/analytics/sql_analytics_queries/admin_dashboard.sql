e333a6d957f94fb8f098bdbda38d707fa66bf077
amit777-code <amitk2k25@gmail.com>
2026-01-02 12:41:40 +0530

    api

--- backend/analytics/sql_analytics_queries/admin_dashboard.sql
+++
@@ -1,5 +0,0 @@
--- Overall analytics
-SELECT
-    COUNT(DISTINCT student_id) AS total_students,
-    AVG(total) AS school_avg_score
-FROM academic_records;
--- backend/analytics/sql_analytics_queries/subject_analytics.sql
+++
@@ -1,6 +0,0 @@
--- Subject wise performance
-SELECT
-    subject,
-    AVG(marks) AS avg_marks
-FROM subject_performance
-GROUP BY subject;
